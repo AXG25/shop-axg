fd90962816ffe10895f94786e2d4b5845909e0dc
AXG25 <axiever.guejia@gmail.com>
2023-01-23 10:30:16 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBarr from "./components/Navbar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" element={<ItemListContainer />} />
         <Route path="/productos/:categoryId" element={<ItemListContainer />}/>
         <Route path="/detail/:itemId" element={<ItemDetailContainer/>}/>
+        <Route path="*" element={<Navigate to={"/"}/>}/>
       </Routes>
       
     </BrowserRouter>
